a268a37c2ff300279ad39978271052ed7e105648
Maxime Legault-Venne <m.legault-venne@metrio.net>
2022-12-04 11:53:06 -0500

    Day 3 challenge 2
    Signed-off-by: Maxime Legault-Venne <m.legault-venne@metrio.net>

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -3,6 +3,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    challenge_1();
+    challenge_2();
+}
+
+fn challenge_1() {
     let lines = read_lines("./input.txt").expect("Should have been able to read the file");
     let priorities: String = String::from(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
@@ -14,6 +19,33 @@ fn main() {
     }).sum::<usize>();
 
     println!("sum of priorities: {}", sum);
+}
+
+fn challenge_2() {
+    let lines = read_lines("./input.txt").expect("Should have been able to read the file");
+    let priorities: String = String::from(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    let mut line_1: String = String::new();
+    let mut line_2: String = String::new();
+    let badges_sum = lines.map(|line_result| {
+        if line_1 == "" {
+            line_1 = line_result.expect("should have been able to read line 1");
+            return 0;
+        }
+        if line_2 == "" {
+            line_2 = line_result.expect("should have been able to read line 2");
+            return 0;
+        }
+        let letters = line_result
+            .expect("should have been able to read line 3")
+            .chars()
+            .filter(|c| line_1.contains(*c) && line_2.contains(*c))
+            .collect::<Vec<char>>();
+        line_1 = String::new();
+        line_2 = String::new();
+        return priorities.find(letters[0]).expect("should have been able to find badge priority");
+    }).sum::<usize>();
+    println!("badges sum: {}", badges_sum);
 }
 
 // The output is wrapped in a Result to allow matching on errors
