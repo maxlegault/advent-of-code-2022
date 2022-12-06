2b036d49ecb17c0128aae0a52160e34ad862f642
Maxime Legault-Venne <m.legault-venne@metrio.net>
2022-12-06 09:01:16 -0500

    Day 6 challenge 2
    Signed-off-by: Maxime Legault-Venne <m.legault-venne@metrio.net>

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,22 +2,27 @@ use std::fs;
 
 fn main() {
     let data = fs::read("./input.txt").expect("should have read file to string");
+    find_marker(&data[0..data.len()], 4);
+    find_marker(&data[0..data.len()], 14);
+}
+
+fn find_marker(data: &[u8], len: usize) {
     let mut found = false;
-    let mut index = 0;
+    let mut index: usize = 0;
     while !found {
-        let marker = &data[index..index+4];
-        if is_marker_valid(marker) {
+        let marker = &data[index..index+len];
+        if is_marker_valid(marker, len) {
             found = true;
         } else {
             index += 1;
         }
     }
-    println!("first marker after character {}", index + 4);
+    println!("first marker of length {} after character {}", len, index + len);
 }
 
-fn is_marker_valid(marker: &[u8]) -> bool {
-    for x in 0..4 {
-        for y in 0..4 {
+fn is_marker_valid(marker: &[u8], len: usize) -> bool {
+    for x in 0..len {
+        for y in 0..len {
             if x != y && marker[x] == marker[y] {
                 return false;
             }
